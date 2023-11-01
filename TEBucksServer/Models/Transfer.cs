02b8e68679945f352d8c28424808f394ec4fd0a9
Ethan May <emay1190@gmail.com>
2023-11-01 11:33:33 -0400

    built models

---
+++ TEBucksServer/Models/Transfer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TEBucksServer.Models
+{
+    public class Transfer
+    {
+        public int TransferId { get; set; }
+        [Required]
+        public int UserFromId { get; set; }
+        [Required]
+        public int UserToId { get; set; }
+        [Required]
+        public string TransferType { get; set; }
+        [Required]
+        public string TransferStatus { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
