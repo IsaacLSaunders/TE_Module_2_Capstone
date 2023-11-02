e5fe844dd5e209a67bb2d94f7ff4f7c44a1bc1bf
Ethan May <emay1190@gmail.com>
2023-11-02 12:34:06 -0400

    built out our TransferSqlDao methods

--- TEBucksServer/DAO/ITransferDao.cs
+++ TEBucksServer/DAO/ITransferDao.cs
@@ -5,7 +5,6 @@ namespace TEBucksServer.DAO
 {
     public interface ITransferDao
     {
-        public List<Transfer> GetAllTransfers();
         public Transfer GetTransferById(int transferId);
         public List<Transfer> GetTransfersByPersonId(int personId);
         public List<Transfer> GetAllTransfersByTypeAndPersonId(int personId, string transferType);
