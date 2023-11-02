f5ac344c54663d518ad1b32818d1ad73327d64d3
Isaac Saunders <IsaacLSaunders@gmail.com>
2023-11-02 17:29:52 -0400

    Isaac Saunders: new db build scripts

--- database/TEBucksBuildScript.sql
+++ database/TEBucksBuildScript.sql
@@ -1,86 +1,6 @@
-USE [master]
-GO
-/****** Object:  Database [tebucks]    Script Date: 11/1/2023 11:39:28 AM ******/
-CREATE DATABASE [tebucks]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'tebucks', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\tebucks.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'tebucks_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\tebucks_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
-GO
-ALTER DATABASE [tebucks] SET COMPATIBILITY_LEVEL = 150
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [tebucks].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [tebucks] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [tebucks] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [tebucks] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [tebucks] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [tebucks] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [tebucks] SET AUTO_CLOSE ON 
-GO
-ALTER DATABASE [tebucks] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [tebucks] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [tebucks] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [tebucks] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [tebucks] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [tebucks] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [tebucks] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [tebucks] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [tebucks] SET  ENABLE_BROKER 
-GO
-ALTER DATABASE [tebucks] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [tebucks] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [tebucks] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [tebucks] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [tebucks] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [tebucks] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [tebucks] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [tebucks] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [tebucks] SET  MULTI_USER 
-GO
-ALTER DATABASE [tebucks] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [tebucks] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [tebucks] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [tebucks] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-ALTER DATABASE [tebucks] SET DELAYED_DURABILITY = DISABLED 
-GO
-ALTER DATABASE [tebucks] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-ALTER DATABASE [tebucks] SET QUERY_STORE = OFF
-GO
 USE [tebucks]
 GO
-/****** Object:  Table [dbo].[Accounts]    Script Date: 11/1/2023 11:39:28 AM ******/
+/****** Object:  Table [dbo].[Accounts]    Script Date: 11/2/2023 5:28:20 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -95,7 +15,7 @@ CREATE TABLE [dbo].[Accounts](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Persons]    Script Date: 11/1/2023 11:39:28 AM ******/
+/****** Object:  Table [dbo].[Persons]    Script Date: 11/2/2023 5:28:20 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -111,13 +31,13 @@ CREATE TABLE [dbo].[Persons](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Transfers]    Script Date: 11/1/2023 11:39:28 AM ******/
+/****** Object:  Table [dbo].[Transfers]    Script Date: 11/2/2023 5:28:20 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Transfers](
-	[TansferId] [int] IDENTITY(1,1) NOT NULL,
+	[TransferId] [int] IDENTITY(1,1) NOT NULL,
 	[UserFromId] [int] NOT NULL,
 	[UserToId] [int] NOT NULL,
 	[TransferType] [varchar](50) NOT NULL,
@@ -125,11 +45,11 @@ CREATE TABLE [dbo].[Transfers](
 	[Amount] [money] NOT NULL,
  CONSTRAINT [PK_Transfers] PRIMARY KEY CLUSTERED 
 (
-	[TansferId] ASC
+	[TransferId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[users]    Script Date: 11/1/2023 11:39:28 AM ******/
+/****** Object:  Table [dbo].[users]    Script Date: 11/2/2023 5:28:20 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -142,12 +62,50 @@ CREATE TABLE [dbo].[users](
  CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED 
 (
 	[user_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
- CONSTRAINT [UQ_username] UNIQUE NONCLUSTERED 
-(
-	[username] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Accounts] ON 
+GO
+INSERT [dbo].[Accounts] ([AccountId], [PersonId], [Balance]) VALUES (2, 12, 1000.0000)
+GO
+INSERT [dbo].[Accounts] ([AccountId], [PersonId], [Balance]) VALUES (3, 13, 1000.0000)
+GO
+SET IDENTITY_INSERT [dbo].[Accounts] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Persons] ON 
+GO
+INSERT [dbo].[Persons] ([Id], [LoginId], [FirstName], [LastName]) VALUES (12, 1015, N'john jacob', N'jinglehimer')
+GO
+INSERT [dbo].[Persons] ([Id], [LoginId], [FirstName], [LastName]) VALUES (13, 1016, N'JImmy', N'hendrix')
+GO
+SET IDENTITY_INSERT [dbo].[Persons] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Transfers] ON 
+GO
+INSERT [dbo].[Transfers] ([TransferId], [UserFromId], [UserToId], [TransferType], [TransferStatus], [Amount]) VALUES (2, 12, 13, N'Send', N'Rejected', 5.0000)
+GO
+INSERT [dbo].[Transfers] ([TransferId], [UserFromId], [UserToId], [TransferType], [TransferStatus], [Amount]) VALUES (3, 12, 13, N'Send', N'Pending', 6.0000)
+GO
+INSERT [dbo].[Transfers] ([TransferId], [UserFromId], [UserToId], [TransferType], [TransferStatus], [Amount]) VALUES (4, 13, 12, N'Send', N'Pending', 25.0000)
+GO
+SET IDENTITY_INSERT [dbo].[Transfers] OFF
+GO
+SET IDENTITY_INSERT [dbo].[users] ON 
+GO
+INSERT [dbo].[users] ([user_id], [username], [password_hash], [salt]) VALUES (1015, N'hisname', N'/cwyj7Xd2AX/q+fiznmwC0USKng=', N'+gQ3wuqJfkY=')
+GO
+INSERT [dbo].[users] ([user_id], [username], [password_hash], [salt]) VALUES (1016, N'purplehaze', N'6WIrdBISF2KT1x0OVmcGt6w6gT4=', N'Rjo9PDrn/48=')
+GO
+SET IDENTITY_INSERT [dbo].[users] OFF
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UQ_username]    Script Date: 11/2/2023 5:28:20 PM ******/
+ALTER TABLE [dbo].[users] ADD  CONSTRAINT [UQ_username] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Accounts]  WITH CHECK ADD  CONSTRAINT [FK_Persons_Accounts] FOREIGN KEY([PersonId])
 REFERENCES [dbo].[Persons] ([Id])
@@ -168,8 +126,4 @@ ALTER TABLE [dbo].[Transfers]  WITH CHECK ADD  CONSTRAINT [FK_Transfers_To_Perso
 REFERENCES [dbo].[Persons] ([Id])
 GO
 ALTER TABLE [dbo].[Transfers] CHECK CONSTRAINT [FK_Transfers_To_Persons]
-GO
-USE [master]
-GO
-ALTER DATABASE [tebucks] SET  READ_WRITE 
 GO
