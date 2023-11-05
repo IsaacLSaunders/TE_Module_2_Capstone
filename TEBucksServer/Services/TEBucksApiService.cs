15582abeefd5195242e0a579024f95bdaba0cc1a
Ethan May <emay1190@gmail.com>
2023-11-05 15:18:59 -0500

    Added services folder

---
+++ TEBucksServer/Services/TEBucksApiService.cs
@@ -0,0 +1,16 @@
+﻿namespace TEBucksServer.NewFolder
+{
+    //public class TEBucksApiService
+    //{
+    //    protected static RestClient client = null;
+    //    private string jwtToken = "01704";
+    //    public TEBucksApiService(string jwtToken)
+    //    {
+    //        if (client == null)
+    //        {
+    //            client = new RestClient(apiUrl);
+    //            client.AddDefaultHeader("APIKEY", apikey);
+    //        }
+    //    }
+    //}
+}
