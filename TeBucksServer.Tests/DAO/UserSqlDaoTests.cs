3ed9d7e2d13aeb74d22f0a97520fbaeb10673a99
Isaac Saunders <IsaacLSaunders@gmail.com>
2023-11-06 11:11:27 -0500

    Isaac Saunders: added test proj, test lasses, bd build scripts

---
+++ TeBucksServer.Tests/DAO/UserSqlDaoTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEBucksServer.DAO;
+using TEBucksServer.Models;
+using TEBucksServer.Security.Models;
+
+namespace TeBucksServer.Tests.DAO
+{
+    [TestClass]
+    public class UserSqlDaoTests : BaseDaoTests
+    {
+
+        private static readonly User USER_1 = new User() { UserId = 1010, FirstName = "joe", LastName = "shmoe", PasswordHash = "qXY8hY+tRMaJNlyHR7bul8Zs4gI=", Salt = "hNkZDaAlH/I=", Username = "username" };
+        private static readonly User USER_2 = new User() { UserId = 1011, FirstName = "joe", LastName = "shmoe", PasswordHash = "B1cY6QY5wqkYrOPwqcliexMVo0U=", Salt = "X2zkWqvduXU=", Username = "username1" };
+        private static readonly User USER_3 = new User() { UserId = 1012, FirstName = "bob", LastName = "bob", PasswordHash = "EpeAL9u572FDuvOwSe3fiWJczqc=", Salt = "ZL7trxpV8qs=", Username = "bob" };
+        private static readonly User USER_4 = new User() { UserId = 1013, FirstName = "jim", LastName = "jim", PasswordHash = "RmszImAgkKUlxb54mCip57JUlVo=", Salt = "9kc0+u06DxA=", Username = "jim" };
+
+        private IUserDao dao;
+
+        private User testUser;
+
+        [TestInitialize]
+        public override void Setup()
+        {
+            dao = new UserSqlDao(ConnectionString);
+            testUser = new User() { UserId = 1014, FirstName = "james", LastName = "james", PasswordHash = "+thi5mQNRmxZ6Jr6hwB4KfZwrIE=", Salt = "BG+0p0QaJ9A=", Username = "james" };
+            base.Setup();
+        }
+
+        [TestMethod]
+        public void GetUserByUserId_HappyPath()
+        {
+            User user = dao.GetUserById(1010);
+            AssertUsersMatch(USER_1, user);
+
+            user = dao.GetUserById(1011);
+            AssertUsersMatch(USER_2, user);
+
+            user = dao.GetUserById(1012);
+            AssertUsersMatch(USER_3, user);
+
+            user = dao.GetUserById(1013);
+            AssertUsersMatch(USER_4, user);
+        }
+
+        [TestMethod]
+        public void GetUserByUserName_HappyPath()
+        {
+            User user = dao.GetUserByUsername("username");
+            AssertUsersMatch(USER_1, user);
+
+            user = dao.GetUserByUsername("username1");
+            AssertUsersMatch(USER_2, user);
+
+            user = dao.GetUserByUsername("bob");
+            AssertUsersMatch(USER_3, user);
+
+            user = dao.GetUserByUsername("jim");
+            AssertUsersMatch(USER_4, user);
+        }
+
+        [TestMethod]
+        public void GetAllUsers_HappyPath()
+        {
+            List<User> expected = new List<User>() { USER_1, USER_2, USER_3, USER_4 };
+            List<User> actual = dao.GetUsers();
+
+            for (int i =0; i< actual.Count; i++)
+            {
+                AssertUsersMatch(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void CreateUser_HappyPath()
+        {
+
+            //NOT SURE IF THIS IS TESTABLE BECAUSE THE HASH AND SALT WILL ALWAYS CHANGE
+            User actual = dao.CreateUser("james", "james", "james", "james");
+            AssertUsersCreatedMatch(testUser, actual);
+        }
+
+        public void AssertUsersMatch(User expected , User actual)
+        {
+            Assert.AreEqual(expected.UserId, actual.UserId);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.PasswordHash, actual.PasswordHash);
+            Assert.AreEqual(expected.Salt, actual.Salt);
+            Assert.AreEqual(expected.Username, actual.Username);
+        }
+
+        public void AssertUsersCreatedMatch(User expected, User actual)
+        {
+            Assert.AreEqual(expected.UserId, actual.UserId);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.Username, actual.Username);
+        }
+    }
+}
---
+++ TeBucksServer.Tests/test-data.sql
@@ -0,0 +1,91 @@
+﻿BEGIN TRANSACTION;
+
+--NEW
+CREATE TABLE [dbo].[accounts](
+	[account_id] [int] IDENTITY(1,1) NOT NULL,
+	[userId] [int] NOT NULL,
+	[balance] [int] NOT NULL,
+ CONSTRAINT [PK_account] PRIMARY KEY CLUSTERED 
+(
+	[account_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+--NEW
+CREATE TABLE [dbo].[transfers](
+	[transferId] [int] IDENTITY(1,1) NOT NULL,
+	[transferType] [varchar](50) NOT NULL,
+	[transferStatus] [varchar](50) NOT NULL,
+	[userFrom] [int] NOT NULL,
+	[userTo] [int] NOT NULL,
+	[amount] [money] NOT NULL,
+ CONSTRAINT [PK_transfer] PRIMARY KEY CLUSTERED 
+(
+	[transferId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+--NEW
+CREATE TABLE [dbo].[users](
+	[user_id] [int] IDENTITY(1001,1) NOT NULL,
+	[firstname] [varchar](50) NOT NULL,
+	[lastname] [varchar](50) NOT NULL,
+	[username] [varchar](50) NOT NULL,
+	[password_hash] [varchar](200) NOT NULL,
+	[salt] [varchar](200) NOT NULL,
+ CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ_username] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+SET IDENTITY_INSERT [dbo].[users] ON
+INSERT [dbo].[users] ([user_id], [firstname], [lastname], [username], [password_hash], [salt]) 
+VALUES 
+(1010, N'joe', N'shmoe', N'username', N'qXY8hY+tRMaJNlyHR7bul8Zs4gI=', N'hNkZDaAlH/I='),
+(1011, N'joe', N'shmoe', N'username1', N'B1cY6QY5wqkYrOPwqcliexMVo0U=', N'X2zkWqvduXU='),
+(1012, N'bob', N'bob', N'bob', N'EpeAL9u572FDuvOwSe3fiWJczqc=', N'ZL7trxpV8qs='),
+(1013, N'jim', N'jim', N'jim', N'RmszImAgkKUlxb54mCip57JUlVo=', N'9kc0+u06DxA=')
+SET IDENTITY_INSERT [dbo].[users] OFF
+
+SET IDENTITY_INSERT [dbo].[accounts] ON
+INSERT [dbo].[accounts] ([account_id], [userId], [balance]) 
+VALUES 
+(10, 1010, 1000),
+(11, 1011, 1000),
+(12, 1012, 1000),
+(13, 1013, 1000)
+SET IDENTITY_INSERT [dbo].[accounts] OFF
+
+SET IDENTITY_INSERT [dbo].[transfers] ON
+INSERT [dbo].[transfers] ([transferId], [transferType], [transferStatus], [userFrom], [userTo], [amount]) 
+VALUES 
+(10, N'Send', N'Approved', 1013, 1012, 5.0000),
+(11, N'Send', N'Approved', 1012, 1013, 5.0000),
+(12, N'Request', N'Pending', 1013, 1012, 10.0000),
+(13, N'Request', N'Pending', 1012, 1013, 10.0000),
+(14, N'Request', N'Approved', 1013, 1012, 15.0000),
+(15, N'Request', N'Approved', 1012, 1013, 15.0000)
+SET IDENTITY_INSERT [dbo].[transfers] OFF
+
+--ALTERS for FK's
+ALTER TABLE [dbo].[accounts]  WITH CHECK ADD  CONSTRAINT [FK_accounts_users] FOREIGN KEY([userId])
+REFERENCES [dbo].[users] ([user_id])
+ALTER TABLE [dbo].[accounts] CHECK CONSTRAINT [FK_accounts_users]
+ALTER TABLE [dbo].[transfers]  WITH CHECK ADD  CONSTRAINT [FK_transfers_from_users] FOREIGN KEY([userFrom])
+REFERENCES [dbo].[users] ([user_id])
+ALTER TABLE [dbo].[transfers] CHECK CONSTRAINT [FK_transfers_from_users]
+ALTER TABLE [dbo].[transfers]  WITH CHECK ADD  CONSTRAINT [FK_transfers_to_users] FOREIGN KEY([userTo])
+REFERENCES [dbo].[users] ([user_id])
+ALTER TABLE [dbo].[transfers] CHECK CONSTRAINT [FK_transfers_to_users]
+ALTER TABLE [dbo].[transfers]  WITH CHECK ADD  CONSTRAINT [CHK_amount] CHECK  (([amount]>(0)))
+ALTER TABLE [dbo].[transfers] CHECK CONSTRAINT [CHK_amount]
+
+
+COMMIT TRANSACTION;
